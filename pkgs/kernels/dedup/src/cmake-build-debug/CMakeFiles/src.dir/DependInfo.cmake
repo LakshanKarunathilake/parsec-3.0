21076c972ac241c92de82ae41f3a5a33267c061b
Lakshan Karunathilake <manualsplash@gmail.com>
2020-01-06 09:08:05 +0530

    Initial Commit

---
+++ pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/binheap.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/binheap.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/decoder.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/decoder.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/dedup.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/dedup.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/encoder.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/encoder.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/hashtable.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/hashtable.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/mbuffer.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/mbuffer.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/queue.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/queue.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/rabin.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/rabin.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/sha.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/sha.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/tree.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/tree.c.obj"
+  "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/util.c" "I:/Research/parsec-benchmark/pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/util.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../openssl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pkgs/kernels/dedup/src/cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/binheap.c.obj"
+  "CMakeFiles/src.dir/decoder.c.obj"
+  "CMakeFiles/src.dir/dedup.c.obj"
+  "CMakeFiles/src.dir/encoder.c.obj"
+  "CMakeFiles/src.dir/hashtable.c.obj"
+  "CMakeFiles/src.dir/mbuffer.c.obj"
+  "CMakeFiles/src.dir/queue.c.obj"
+  "CMakeFiles/src.dir/rabin.c.obj"
+  "CMakeFiles/src.dir/sha.c.obj"
+  "CMakeFiles/src.dir/tree.c.obj"
+  "CMakeFiles/src.dir/util.c.obj"
+  "libsrc.dll.a"
+  "src.exe"
+  "src.exe.manifest"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
